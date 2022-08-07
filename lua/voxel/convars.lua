957045c377659ee793dd519bcd8f1962a11155b7
TankNut <m.vlasblom@live.nl>
2022-08-07 18:54:49 +0200

    Kind of expand alpha support (but also not really)

--- lua/voxel/convars.lua
+++ lua/voxel/convars.lua
@@ -13,4 +13,5 @@ if CLIENT then
 	voxel.Convars.ColorR = CreateClientConVar("voxel_col_r", 255, true, true, "", 0, 255)
 	voxel.Convars.ColorG = CreateClientConVar("voxel_col_g", 255, true, true, "", 0, 255)
 	voxel.Convars.ColorB = CreateClientConVar("voxel_col_b", 255, true, true, "", 0, 255)
+	voxel.Convars.ColorA = CreateClientConVar("voxel_col_a", 255, true, true, "", 0, 255)
 end
