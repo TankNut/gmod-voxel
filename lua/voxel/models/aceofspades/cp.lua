4f6ddcd9c4126767432faa75c880bf2d43eb63c4
TankNut <m.vlasblom@live.nl>
2022-08-11 22:15:32 +0200

    more models

---
+++ lua/voxel/models/aceofspades/cp.lua
@@ -0,0 +1,6 @@
+return {
+	Mesh = "cp",
+	UseMeshBounds = true,
+
+	Offset = Vector(0, 0, 0.5)
+}
---
+++ lua/voxel/models/aceofspades/intel.lua
@@ -0,0 +1,6 @@
+return {
+	Mesh = "intel",
+	UseMeshBounds = true,
+
+	Offset = Vector(0, 0.5, 0.5)
+}
