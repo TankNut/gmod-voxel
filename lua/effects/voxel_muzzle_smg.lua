e2c52e01f155821663e1452a3828c3e0e697836f
TankNut <m.vlasblom@live.nl>
2023-12-16 04:07:26 +0100

    Some voxel effects

---
+++ lua/effects/voxel_muzzle_smg.lua
@@ -0,0 +1,151 @@
+--[[
+SDK References: 
+
+CTempEnts::MuzzleFlash_SMG1_Player
+FX_MuzzleEffectAttached
+--]]
+
+EFFECT.VMMats = {}
+EFFECT.WMMats = {}
+
+for i = 1, 4 do
+	EFFECT.VMMats[i] = Material("effects/muzzleflash" .. i .. "_noz")
+	EFFECT.WMMats[i] = Material("effects/muzzleflash" .. i)
+end
+
+local function translatefov(ent, pos, inverse)
+	local worldx = math.tan(LocalPlayer():GetFOV() * (math.pi / 360))
+	local viewx = math.tan(ent.ViewModelFOV * (math.pi / 360))
+
+	local factor = Vector(worldx / viewx, worldx / viewx, 0)
+	local tmp = pos - EyePos()
+
+	local eye = EyeAngles()
+	local transformed = Vector(eye:Right():Dot(tmp), eye:Up():Dot(tmp), eye:Forward():Dot(tmp))
+
+	if inverse then
+		transformed.x = transformed.x / factor.x
+		transformed.y = transformed.y / factor.y
+	else
+		transformed.x = transformed.x * factor.x
+		transformed.y = transformed.y * factor.y
+	end
+
+	local out = (eye:Right() * transformed.x) + (eye:Up() * transformed.y) + (eye:Forward() * transformed.z)
+
+	return EyePos() + out
+end
+
+function EFFECT:Init(data)
+	self.Ent = data:GetEntity()
+	self.Scale = data:GetScale()
+
+	if self.Ent:IsCarriedByLocalPlayer() then
+		self:ParticleInitVM()
+	end
+
+	self:ParticleInitWM()
+end
+
+function EFFECT:ParticleInitVM()
+	self.EmitterVM = ParticleEmitter(Vector())
+	self.EmitterVM:SetNoDraw(true)
+
+	local forward = Vector(1, 0, 0)
+	local scale = math.Rand(1.25, 1.5) * self.Scale
+
+	for i = 1, 6 do
+		local offset = forward * (i * 8 * scale)
+		local p = self.EmitterVM:Add(table.Random(self.VMMats), offset)
+
+		p:SetDieTime(0.025)
+
+		p:SetColor(255, 255, math.random(200, 255))
+
+		local size = (math.Rand(6, 8) * (8 - i) / 6) * scale
+
+		p:SetStartSize(size)
+		p:SetEndSize(size)
+
+		p:SetRoll(math.random(0, 360))
+	end
+end
+
+function EFFECT:ParticleInitWM()
+	self.EmitterWM = ParticleEmitter(Vector())
+	self.EmitterWM:SetNoDraw(true)
+
+	local forward = Vector(1, 0, 0)
+	local scale = math.Rand(self.Scale - 0.25, self.Scale + 0.25)
+
+	for i = 1, 9 do
+		local offset = forward * (i * 2 * scale)
+		local p = self.EmitterWM:Add(table.Random(self.WMMats), offset)
+
+		p:SetDieTime(0.025)
+
+		p:SetStartAlpha(255)
+		p:SetEndAlpha(128)
+
+		local size = (math.Rand(6, 9) * (12 - i) / 9) * scale
+
+		p:SetStartSize(size)
+		p:SetEndSize(size)
+
+		p:SetRoll(math.random(0, 360))
+	end
+end
+
+function EFFECT:IsDrawingVM()
+	return self.Ent:IsCarriedByLocalPlayer() and not LocalPlayer():ShouldDrawLocalPlayer()
+end
+
+function EFFECT:GetStartPos(ent)
+	local pos, ang = ent:GetVoxelModel():GetAttachment("muzzle")
+
+	if self:IsDrawingVM() then
+		pos, ang = LocalToWorld(pos * ent.VoxelData.Scale, ang, ent:GetViewPos())
+
+		return translatefov(ent, pos), ang
+	else
+		return LocalToWorld(pos * ent.VoxelData.Scale, ang, ent:GetWorldPos())
+	end
+end
+
+function EFFECT:Think()
+	local live = false
+
+	if not IsValid(self.Ent) then
+		return false
+	end
+
+	for _, v in pairs({self.EmitterVM, self.EmitterWM}) do
+		if v and v:IsValid() then
+			if v:GetNumActiveParticles() == 0 then
+				v:Finish()
+			else
+				live = true
+			end
+		end
+	end
+
+	return live
+end
+
+function EFFECT:Render()
+	local pos, ang = self:GetStartPos(self.Ent)
+
+	self:SetPos(pos)
+
+	cam.Start3D(WorldToLocal(EyePos(), EyeAngles(), pos, ang))
+		local emitter = self.EmitterWM
+
+		if self:IsDrawingVM() then
+			emitter = self.EmitterVM
+		end
+
+		if emitter and emitter:IsValid() then
+			emitter:Draw()
+		end
+	cam.End3D()
+end
---
+++ lua/effects/voxel_tracer_smoke.lua
@@ -0,0 +1,103 @@
+EFFECT.Mat = Material("trails/smoke")
+
+local function translatefov(ent, pos, inverse)
+	local worldx = math.tan(LocalPlayer():GetFOV() * (math.pi / 360))
+	local viewx = math.tan(ent.ViewModelFOV * (math.pi / 360))
+
+	local factor = Vector(worldx / viewx, worldx / viewx, 0)
+	local tmp = pos - EyePos()
+
+	local eye = EyeAngles()
+	local transformed = Vector(eye:Right():Dot(tmp), eye:Up():Dot(tmp), eye:Forward():Dot(tmp))
+
+	if inverse then
+		transformed.x = transformed.x / factor.x
+		transformed.y = transformed.y / factor.y
+	else
+		transformed.x = transformed.x * factor.x
+		transformed.y = transformed.y * factor.y
+	end
+
+	local out = (eye:Right() * transformed.x) + (eye:Up() * transformed.y) + (eye:Forward() * transformed.z)
+
+	return EyePos() + out
+end
+
+function EFFECT:Init(data)
+	self.Ent = data:GetEntity()
+
+	self.Start = self:GetStartPos(self.Ent)
+	self.End = data:GetOrigin()
+	self.Normal = (self.End - self.Start):GetNormalized()
+
+	self.Length = self.Start:Distance(self.End)
+
+	self:SetRenderBoundsWS(self.Start, self.End)
+
+	self.StartTime = CurTime()
+	self.DieTime = CurTime() + 0.1
+end
+
+function EFFECT:IsDrawingVM()
+	return self.Ent:IsCarriedByLocalPlayer() and not LocalPlayer():ShouldDrawLocalPlayer()
+end
+
+function EFFECT:GetStartPos(ent)
+	local pos, ang = ent:GetVoxelModel():GetAttachment("muzzle")
+
+	if self:IsDrawingVM() then
+		pos, ang = LocalToWorld(pos * ent.VoxelData.Scale, ang, ent:GetViewPos())
+
+		return translatefov(ent, pos), ang
+	else
+		return LocalToWorld(pos * ent.VoxelData.Scale, ang, ent:GetWorldPos())
+	end
+end
+
+function EFFECT:Think()
+	if not self.DieTime or CurTime() > self.DieTime then
+		return false
+	end
+
+	return true
+end
+
+local dirs = {
+	Vector(1, 0, 0),
+	Vector(-1, 0, 0),
+	Vector(0, 1, 0),
+	Vector(0, -1, 0),
+	Vector(0, 0, 1),
+	Vector(0, 0, -1)
+}
+
+local function getLighting(vec)
+	local col = Vector()
+
+	for _, v in pairs(dirs) do
+		col = col + render.ComputeLighting(vec, v)
+	end
+
+	col:Div(6)
+
+	local len = col:Length()
+
+	return Color(len * 255, len * 255, len * 255)
+end
+
+local size = 2
+local maxAlpha = 100
+
+function EFFECT:Render()
+	local life = math.TimeFraction(self.StartTime, self.DieTime, CurTime())
+	local alpha = maxAlpha - life * maxAlpha
+	local dist = self.Length / size
+
+	render.SetMaterial(self.Mat)
+	render.StartBeam(size + 1)
+		for i = 0, size do
+			local pos = self.Start + (self.Normal * dist) * i
+			render.AddBeam(pos, 5 + 10 * life, 0, ColorAlpha(getLighting(pos), alpha))
+		end
+	render.EndBeam()
+end
