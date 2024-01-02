c4a799d7b4fb1b3a75a628748b43d9628ef15301
TankNut <m.vlasblom@live.nl>
2024-01-03 00:56:02 +0100

    Basic holographic sight support

--- lua/weapons/spades_base/sh_view.lua
+++ lua/weapons/spades_base/sh_view.lua
@@ -1,4 +1,5 @@
 AddCSLuaFile()
+DEFINE_BASECLASS("voxel_swep_base")
 
 function SWEP:GetAimFraction()
 	return math.pow(math.sin(self:GetAimState() * math.pi * 0.5), math.pi)
@@ -117,5 +118,49 @@ if CLIENT then
 
 	function SWEP:ShouldHideViewModel()
 		return self:IsReloading()
+	end
+
+	function SWEP:DrawVoxelModel()
+		BaseClass.DrawVoxelModel(self)
+
+		--self:DrawHolosight(Vector(-1, 0, 5), Vector(-1, 0, 0), 100, 1, 1, Color(0, 0, 0, 50), 1, 1, Color(255, 0, 0))
+	end
+
+	function SWEP:DrawHolosight(pos, normal, distance, glassWidth, glassHeight, glassColor, sightWidth, sightHeight, sightColor, sightMaterial)
+		if halo.RenderedEntity() == self then
+			return
+		end
+
+		render.SetColorMaterial()
+
+		render.SetStencilWriteMask(0xFF)
+		render.SetStencilTestMask(0xFF)
+
+		render.SetStencilReferenceValue(0)
+
+		render.SetStencilPassOperation(STENCIL_REPLACE)
+		render.SetStencilFailOperation(STENCIL_KEEP)
+		render.SetStencilZFailOperation(STENCIL_KEEP)
+
+		render.ClearStencil()
+
+		render.SetStencilEnable(true)
+
+		render.SetStencilReferenceValue(1)
+		render.SetStencilCompareFunction(STENCIL_ALWAYS)
+
+		-- Draw mask
+		render.DrawQuadEasy(pos, normal, glassWidth, glassHeight, glassColor)
+
+		render.SetStencilCompareFunction(STENCIL_EQUAL)
+
+		-- Draw contents
+		if sightMaterial then
+			render.SetMaterial(sightMaterial)
+		end
+
+		render.DrawQuadEasy(pos + -normal * distance, normal, sightWidth * (distance / 100), sightHeight * (distance / 100), Color(255, 0, 0))
+
+		render.SetStencilEnable(false)
 	end
 end
