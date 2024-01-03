1dea7ec030e3e4fec8c1c6dab7fdb7316f4d0f70
TankNut <m.vlasblom@live.nl>
2024-01-03 01:10:30 +0100

    Basic eotech material

--- lua/weapons/spades_base/sh_view.lua
+++ lua/weapons/spades_base/sh_view.lua
@@ -120,10 +120,12 @@ if CLIENT then
 		return self:IsReloading()
 	end
 
+	local mat = Material("reticles/eotech")
+
 	function SWEP:DrawVoxelModel()
 		BaseClass.DrawVoxelModel(self)
 
-		--self:DrawHolosight(Vector(-1, 0, 5), Vector(-1, 0, 0), 100, 1, 1, Color(0, 0, 0, 50), 1, 1, Color(255, 0, 0))
+		--self:DrawHolosight(Vector(-1, 0, 5), Vector(-1, 0, 0), 100, 1, 1, Color(0, 0, 0, 50), 2, 2, Color(255, 0, 0), mat)
 	end
 
 	function SWEP:DrawHolosight(pos, normal, distance, glassWidth, glassHeight, glassColor, sightWidth, sightHeight, sightColor, sightMaterial)
